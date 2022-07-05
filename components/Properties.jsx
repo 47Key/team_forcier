30b6926f099998b1ca009b4d0c49c5e3b7d183a6
47key <rkeegan@live.com>
2022-07-05 13:02:49 -0400

    components all working together, getStaticProps moved to index.js with <Properties /> component. Background css fixed, responsive layout working. Needs styling and a featured properties section still.

--- components/Properties.jsx
+++ components/Properties.jsx
@@ -1,46 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import PropertiesByCity from './pull-city';
-// import Properties from './pull_data';
+import PropertiesDefault from './pull_data';
 import styles from '../styles/Properties.module.css';
 
-export const getStaticProps = async () => {
-  const responseWin = await fetch(`https://key47-51636.firebaseio.com/Properties.json?orderBy="/records/0/Address/City"&equalTo="Windsor"`);
-  const getDataWin = await responseWin.text();
-  const parsedDataWin = JSON.parse(getDataWin);
-  const keyNameWin = Object.entries(parsedDataWin);
-
-  const responseTec = await fetch(`https://key47-51636.firebaseio.com/Properties.json?orderBy="/records/0/Address/City"&equalTo="Tecumseh"`);
-  const getDataTec = await responseTec.text();
-  const parsedDataTec = JSON.parse(getDataTec);
-  const keyNameTec = Object.entries(parsedDataTec);
-
-  const responseEss = await fetch(`https://key47-51636.firebaseio.com/Properties.json?orderBy="/records/0/Address/City"&equalTo="Essex"`);
-  const getDataEss = await responseEss.text();
-  const parsedDataEss = JSON.parse(getDataEss);
-  const keyNameEss = Object.entries(parsedDataEss);
-
-  const responseLas = await fetch(`https://key47-51636.firebaseio.com/Properties.json?orderBy="/records/0/Address/City"&equalTo="Lasalle"`);
-  const getDataLas = await responseLas.text();
-  const parsedDataLas = JSON.parse(getDataLas);
-  const keyNameLas = Object.entries(parsedDataLas);
-
-  const responseAmh = await fetch(`https://key47-51636.firebaseio.com/Properties.json?orderBy="/records/0/Address/City"&equalTo="Amherstburg"`);
-  const getDataAmh = await responseAmh.text();
-  const parsedDataAmh = JSON.parse(getDataAmh);
-  const keyNameAmh = Object.entries(parsedDataAmh);
-  
-  return {
-    props: { 
-      propertiesWin: keyNameWin,
-      propertiesTec: keyNameTec, 
-      propertiesEss: keyNameEss, 
-      propertiesLas: keyNameLas, 
-      propertiesAmh: keyNameAmh,  
-    }
-  }
-}
-
-export default function PropertyGrid({ propertiesWin, propertiesTec, propertiesLas, propertiesAmh, propertiesEss }) {
+export default function PropertyGrid({ propertiesWin, propertiesTec, propertiesLas, propertiesAmh, propertiesEss, propertiesDef }) {
   const [city, setCity] = useState('Default');
 
   const [defaultContentVisible, setDefaultContentVisible] = useState(true);
@@ -78,21 +41,7 @@ export default function PropertyGrid({ propertiesWin, propertiesTec, propertiesL
       </div>
       {defaultContentVisible && 
       <div className={styles.propwrap}>
-        {/* <Properties id={3}/>
-        <Properties id={4}/>
-        <Properties id={5}/>
-        <Properties id={6}/>
-        <Properties id={7}/>
-        <Properties id={8}/>
-        <Properties id={9}/>
-        <Properties id={13}/>
-        <Properties id={14}/>
-        <Properties id={15}/>
-        <Properties id={16}/>
-        <Properties id={17}/>
-        <Properties id={18}/>
-        <Properties id={19}/>
-        <Properties id={20}/> */}
+        <PropertiesDefault properties={propertiesDef} />
       </div>
       }
       {windsorContentVisible && 
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,51 @@ import Image from 'next/image'
 import NavBar from '../components/navbar'
 import Hero from '../components/hero'
 import styles from '../styles/Home.module.css'
+import PropertyGrid from '../components/Properties'
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const responseDef = await fetch(`https://key47-51636.firebaseio.com/Properties.json?orderBy="$key"&limitToFirst=1`);
+  const getDataDef = await responseDef.text();
+  const parsedDataDef = JSON.parse(getDataDef);
+  const keyNameDef = Object.entries(parsedDataDef);
+
+  const responseWin = await fetch(`https://key47-51636.firebaseio.com/Properties.json?orderBy="/records/0/Address/City"&equalTo="Windsor"`);
+  const getDataWin = await responseWin.text();
+  const parsedDataWin = JSON.parse(getDataWin);
+  const keyNameWin = Object.entries(parsedDataWin);
+
+  const responseTec = await fetch(`https://key47-51636.firebaseio.com/Properties.json?orderBy="/records/0/Address/City"&equalTo="Tecumseh"`);
+  const getDataTec = await responseTec.text();
+  const parsedDataTec = JSON.parse(getDataTec);
+  const keyNameTec = Object.entries(parsedDataTec);
+
+  const responseEss = await fetch(`https://key47-51636.firebaseio.com/Properties.json?orderBy="/records/0/Address/City"&equalTo="Essex"`);
+  const getDataEss = await responseEss.text();
+  const parsedDataEss = JSON.parse(getDataEss);
+  const keyNameEss = Object.entries(parsedDataEss);
+
+  const responseLas = await fetch(`https://key47-51636.firebaseio.com/Properties.json?orderBy="/records/0/Address/City"&equalTo="Lasalle"`);
+  const getDataLas = await responseLas.text();
+  const parsedDataLas = JSON.parse(getDataLas);
+  const keyNameLas = Object.entries(parsedDataLas);
+
+  const responseAmh = await fetch(`https://key47-51636.firebaseio.com/Properties.json?orderBy="/records/0/Address/City"&equalTo="Amherstburg"`);
+  const getDataAmh = await responseAmh.text();
+  const parsedDataAmh = JSON.parse(getDataAmh);
+  const keyNameAmh = Object.entries(parsedDataAmh);
+  
+  return {
+    props: { 
+      propertiesWin: keyNameWin,
+      propertiesTec: keyNameTec, 
+      propertiesEss: keyNameEss, 
+      propertiesLas: keyNameLas, 
+      propertiesAmh: keyNameAmh,
+      propertiesDef: keyNameDef,  
+    }
+  }
+}
+export default function Home({ propertiesWin, propertiesTec, propertiesEss, propertiesLas, propertiesAmh, propertiesDef }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -15,18 +58,10 @@ export default function Home() {
 
       <NavBar />
       <Hero />
+      <PropertyGrid propertiesDef={propertiesDef} propertiesWin={propertiesWin} propertiesTec={propertiesTec} propertiesLas={propertiesLas} propertiesAmh={propertiesAmh} propertiesEss={propertiesEss} />
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <h1>Work by Key the Dev</h1>
       </footer>
     </div>
   )
